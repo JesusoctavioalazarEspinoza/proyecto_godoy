e90fbd8b00a4132b6721c5a2807abffe616cfa6f
JavierC2003 <elcholo50.antrax@gmail.com>
2024-11-25 17:46:57 -0700

    Update cliente.php
    linia 12 css

--- app/views/layouts/cliente.php
+++ app/views/layouts/cliente.php
@@ -9,7 +9,7 @@
     <!-- CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <link href="<?php echo url('css/cliente.css'); ?>" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/styles.css">
 </head>
 <body>
     <!-- Navbar -->
