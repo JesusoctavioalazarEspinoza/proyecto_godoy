4028ba396056cd3b9cdbe2e5027f552093289d83
JavierC2003 <elcholo50.antrax@gmail.com>
2024-11-25 17:47:36 -0700

    Update main.php
    linia 11 css

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -8,7 +8,7 @@
     
     <!-- CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/css/styles.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/styles.css">
     
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
